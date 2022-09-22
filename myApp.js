203b4a142a29fd80492bd13a6c3d407511d63fce
Mike Bennett <mvbennett9@gmail.com>
2022-09-23 01:44:43 +0900

    find edit save worked

--- myApp.js
+++ myApp.js
@@ -71,7 +71,16 @@ const findPersonById = (personId, done) => {
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  Person.findById(personId, (err, person) => {
+    if (err) return console.error(err);
+
+    person.favoriteFoods.push(foodToAdd);
+
+    person.save((err, data) => {
+      if (err) return console.error(err);
+      done(null, data);
+    })
+  })
 };
 
 const findAndUpdate = (personName, done) => {
