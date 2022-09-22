b72cfaa78b46a5db3d5362150204d938c0ae7018
Mike Bennett <mvbennett9@gmail.com>
2022-09-23 01:34:27 +0900

    find by Id works

--- myApp.js
+++ myApp.js
@@ -45,16 +45,27 @@ const createManyPeople = (arrayOfPeople, done) => {
   })
 };
 
+let personName = 'mike';
+
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  })
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food}, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  })
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) return console.error(err)
+    done(null, data);
+  })
 };
 
 const findEditThenSave = (personId, done) => {
