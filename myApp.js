8ade97acbf0e2224d53eb736514705071db31ef7
Mike Bennett <mvbennett9@gmail.com>
2022-09-23 11:31:51 +0900

    changed to findByIdAndDelete

--- myApp.js
+++ myApp.js
@@ -94,7 +94,7 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  Person.findByIdAndRemove(personId, (err, data) => {
+  Person.findByIdAndDelete(personId, (err, data) => {
     if (err) return console.error(err);
 
     done(null, data);
